b6b0d33e845d4683e6985a8a64f2240b5f00f3ad
Khral Steelforge <garuda2550@gmail.com>
2017-01-30 00:06:13 +0700

    Remove unneeded include.
    Signed-off-by: Khral Steelforge <garuda2550@gmail.com>

--- src/BrowserWindow.cpp
+++ src/BrowserWindow.cpp
@@ -39,7 +39,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "ProgressStatusBar.h"
 #include "PreviewPanel.h"
 #include "PreviewGLCanvas.h"
-#include "version.h"
 
 #include "Tasks/ReadIndexTask.h"
 #include "Tasks/ScanDatTask.h"
