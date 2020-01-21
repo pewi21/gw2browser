d48d67d6c7e1d1cfe5a5ab4023b6523503a30c2f
Dominik Kosík <dominik6kosik@gmail.com>
2020-01-21 10:11:04 +0100

    Fix when materialIndex is returned as -1 to not go out of bounds.

--- src/Exporter.cpp
+++ src/Exporter.cpp
@@ -750,6 +750,8 @@ namespace gw2b {
         for ( uint i = 0; i < model.numMeshes( ); i++ ) {
             const GW2Mesh& mesh = model.mesh( i );
 
+			if (mesh.materialIndex == -1) continue;
+
             materialName[mesh.materialIndex] = mesh.materialName.c_str( );
         }
 
